ee318ac59ef1798ad0601eaa53dc1e287e2f041c
HariPrasath S <115528525+Hariprasath005@users.noreply.github.com>
2023-05-08 15:18:01 +0530

    Add files via upload

---
+++ Curd_Spring1/src/main/java/com/example/demo/CurdSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdSpring1Application.class, args);
+	}
+
+}
